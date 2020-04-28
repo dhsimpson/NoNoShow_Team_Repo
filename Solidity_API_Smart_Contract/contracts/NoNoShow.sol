54af06f130a7c61f311b1f56b33aae0b833bb0bf
dhsimpson <qeadzc9503@naver.com>
2020-04-29 05:04:13 +0900

    모든 함수를 구현 및 불필요한 함수, mapping변수 제거(custBooks관련). API문서 수정이 필요함

--- Solidity_API_Smart_Contract/contracts/NoNoShow.sol
+++ Solidity_API_Smart_Contract/contracts/NoNoShow.sol
@@ -5,15 +5,13 @@ contract NoNoShow{
   // DB 이외의 파라미터
   address payable creator;
   struct books{
-    bytes32 bookID; // 현 예약의 정보들을 토대로 keccak돌린거
-    string compID
+    string compID;
     string compName; // 매장명
     string keyID;
     string custName; // 고객 이름
     string custPN; // 고객 전화번호
     uint32 date; // 20201414같이
     uint32 time; // 1130같이
-    bool isbooked; // 예약됐는지
     bool isShow; // 노쇼면false 쇼면true
   }
 
@@ -23,59 +21,109 @@ contract NoNoShow{
     string phoneNumber;
     string name;
     string ID;
-    string password;
+    string PW;
     uint32 showCnt;
     uint32 noShowCnt;
     // 토큰, 쿠폰 관련은 나중에 다룸
   }
   struct compDB{//comp 는 company
     string compID;
+    string comName;
     string ID;
-    string password;
+    string PW;
     string addr; // 매장 주소
     string phoneNumber; // 매장 대표번호
     // 멤버십 등록 같은 사항은 아직 미정
   }
+  mapping(string=>custDB) userCust;// 고객 회원 DB
+  mapping(string=>compDB) userComp;// 업체 회원 DB
   mapping(string=>books[]) LedgerDB;// 모든 예약기록을 담아 두는 핵심 DB
   mapping(string=>books[]) compBooks;// c.f.) 각 업체의 최근 예약 리스트를 갖고 있는 DB
+  books[] resBooks; // 함수에서 예약목록 배열을 반환하기 위해 사용
   // 이벤트 함수들 (예약 혹은 예약 확인 시 프론트엔드로 정보를 전달한다.)
-  event alertToComp(string keyID,string compID,bytes32 bookID, uint32 date, uint32 time);
-  event alertToCust(string keyID,bytes32 bookID, bool ack);
+  event alertToComp(string keyID,string compID, uint32 date, uint32 time);
+  event alertToCust(string keyID, bool ack);
+  // 생성자
+  constructor () public{
+    creator = msg.sender; //스마트 컨트랙트 생성 시에 creator 변수에 생성자 지갑 주소를 할당한다.
+  }
   // 고객
-  function custSignIn(string memory name,string memory phoneNumber,string memory id, string memory pw) public returns(bool){
-
+  function custSignIn(string memory phoneNumber,string memory name,string memory id, string memory pw) public returns(bool){
+    if(bytes(userCust[phoneNumber].ID).length!=0){//회원가입 된 전번인지 확인, 회원이 있을 땐 ID길이가 0이 아님을 이용
+      //c.f.) 회원 전번이 바뀌는 경우는 delete userCust[phoneNumber] 하면 될듯
+      string memory str = "1234";
+      string memory keyID = string(abi.encodePacked(phoneNumber,str));
+      userCust[phoneNumber] = custDB(keyID,phoneNumber,name,id,pw,0,0); // keyID는 임의로, 전번+"1234" 로 한다.
+      return true;
+    }
+    return false;
   }
-  function custLogIn(string memory id, string memory pw) public view returns(string keyID,bool pass){//
-
+  function custLogIn(string memory phoneNumber, string memory id, string memory pw) public view returns(string memory keyID,bool pass){//
+    if(keccak256(abi.encodePacked((userCust[phoneNumber].ID)))  == keccak256(abi.encodePacked((id))) && keccak256(abi.encodePacked((userCust[phoneNumber].PW))) == keccak256(abi.encodePacked((pw)))){
+      return (userCust[phoneNumber].keyID,true);
+    }
+    return ("",false);
   }
-  function book(string memory keyID,string memory compID,uint32 memory date, uint32 memory time)public{
-    alertToComp(keyID,compID,bookID,date,time);
-  }
-  function checkBook(string memory keyID) public view returns(books[]){ // 예약됐는지 확인
-
+  function book(string memory keyID,string memory compID,uint32 date, uint32 time)public{
+    emit alertToComp(keyID,compID,date,time); // 예약 내용을 보내주기만 한다. 실제 Tx 생성은 업체가 한다.
   }
   // 업체
-  function compSignIn(string memory name,string memory phoneNumber,string memory addr, string memory id, string memory pw) public returns(bool){
-
+  function compSignIn(string memory compID, string memory compName, string memory id, string memory pw ,string memory addr,string memory phoneNumber) public returns(bool){
+    if(bytes(userComp[phoneNumber].ID).length!=0){//회원가입 된 전번인지 확인, 회원이 있을 땐 ID길이가 0이 아님을 이용
+      //c.f.) 회원 전번이 바뀌는 경우는 delete userCust[phoneNumber] 하면 될듯
+      userComp[phoneNumber] = compDB(compID, compName, id, pw, addr, phoneNumber); // keyID는 임의로, 전번+"1234" 로 한다.
+      return true;
+    }
+    return false;
   }
+  //function changeCompInfo() => 고객정보 변경 함수가 불필요하게 복잡하기에 추후에 만들기로 한다.
 
-  function compLogIn() public view returns(bool pass){
-
+  function compLogIn(string memory phoneNumber, string memory id, string memory pw) public view returns(bool pass){ // 원래는 로그인할떄 세션값 넘겨져야하는데..ㅎㅎ
+    if(keccak256(abi.encodePacked((userComp[phoneNumber].ID)))  == keccak256(abi.encodePacked((id))) && keccak256(abi.encodePacked((userComp[phoneNumber].PW))) == keccak256(abi.encodePacked((pw)))){
+      return (true); // 두 문자열을 비교하기 위해 keccack256 사용이 필수이다.(string 자체로는 주솟값이므로)
+    }
+    return (false);
   }
   ////업체의 예약 관련 함수
-  function bookList(string memory compID,uint32 memory date) public view returns(books[] memory){ // 업체 ID와 날짜로 그날의 예약 리스트 뽑아오기
-
+  function bookList(string memory compID,uint32 date) public returns(books[] memory){ // 업체 ID와 날짜로 그날의 예약 리스트 뽑아오기
+    delete resBooks;
+    for(uint32 i = uint32(compBooks[compID].length-1);i>=0;i--){
+        if(compBooks[compID][i].date == date){
+            resBooks.push(compBooks[compID][i]);
+        }
+    }
+    return resBooks;
   }
-  function ackBook(string memory keyID, bytes32 memory bookID, bool memory ack) public{
-    alertToCust(keyID, bookID, ack);
+  function ackBook(string memory compID,string memory compName,string memory keyID,string memory custName, string memory custPN, uint32 date,uint32 time, bool ack) public{
+    LedgerDB[keyID].push(books(compID,compName,keyID,custName,custPN,date,time,false));
+    compBooks[compID].push(books(compID,compName,keyID,custName,custPN,date,time,false));
+    emit alertToCust(keyID, ack);
   }
-  function resBook(bytes32 memory bookID, bool memory isShow) public{ // 고객이 예약시간에 왔는지 안왔는지 확인
-
+  function resBook(string memory keyID,string memory compID, uint32 date, bool isShow) public{ // 고객이 예약시간에 왔는지 안왔는지 확인
+    for(uint32 i = uint32(LedgerDB[keyID].length - 1);i>=0;i--){
+        if(LedgerDB[keyID][i].date == date){
+            LedgerDB[keyID][i].isShow = isShow;
+            break;
+        }
+    }
+    for(uint32 i = uint32(compBooks[compID].length - 1);i>=0;i--){
+        if(compBooks[compID][i].date == date){
+            compBooks[compID][i].isShow = isShow;
+            break;
+        }
+    }
   }
-  function checkUser(string memory phoneNumber) public view returns(books[] memory){ // 고객의 최근 노쇼 기록을 확인
-
+  function checkUser(string memory phoneNumber,uint32 idx) public returns(books[] memory){ // 고객의 최근 노쇼 기록을 확인
+    delete resBooks;
+    uint32 bookLength = uint32(LedgerDB[userCust[phoneNumber].keyID].length);
+    for(uint32 i = bookLength - 1;i>bookLength-1-idx || i>=0;i--){
+        resBooks.push(LedgerDB[userCust[phoneNumber].keyID][i]);
+    }
+    return resBooks;
   }
-  function callBook(string memory keyID,string memory compID,uint32 memory date, uint32 memory time) public returns(bool isbooked) {//전화 예약에 대해 업체가 예약 넣는것
-
+  function callBook(string memory compID,string memory compName,string memory keyID,string memory custName, string memory custPN, uint32 date,uint32 time) public {//전화 예약에 대해 업체가 예약 넣는것
+    LedgerDB[keyID].push(books(compID,compName,keyID,custName,custPN,date,time,false));
+    compBooks[compID].push(books(compID,compName,keyID,custName,custPN,date,time,false));
+    emit alertToCust(keyID, true);
   }
 }
