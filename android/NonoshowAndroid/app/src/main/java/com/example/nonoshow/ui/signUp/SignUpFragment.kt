5ab83de54e9d3cfa3c024580d080d549cc5830a3
박해범 <beom0528@naver.com>
2020-05-14 07:55:49 +0900

    이더리움 method - JS function을 java에서 실행

--- android/NonoshowAndroid/app/src/main/java/com/example/nonoshow/ui/signUp/SignUpFragment.kt
+++ android/NonoshowAndroid/app/src/main/java/com/example/nonoshow/ui/signUp/SignUpFragment.kt
@@ -2,6 +2,7 @@ package com.example.nonoshow.ui.signUp
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,10 +10,14 @@ import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.findNavController
+import com.example.nonoshow.EthereumServiceKt.Companion.callMethodCompSignUp
+import com.example.nonoshow.EthereumServiceKt.Companion.callMethodCustSignUp
 import com.example.nonoshow.MyApplication
 import com.example.nonoshow.MyApplication.Companion.managerMode
 import com.example.nonoshow.R
 import kotlinx.android.synthetic.main.fragment_sign_up.*
+import org.web3j.abi.datatypes.Uint
+import org.web3j.abi.datatypes.generated.Uint32
 
 class SignUpFragment : Fragment() { /*회원가입*/
 
@@ -21,9 +26,26 @@ class SignUpFragment : Fragment() { /*회원가입*/
         super.onViewCreated(view, savedInstanceState)
         MyApplication.isLogined = false
         val signUp : Button = getView()!!.findViewById(R.id.button_signup)
-
         signUp.setOnClickListener{
             MyApplication.isLogined = false
+            val id = nickName.text.toString()
+            val name = Name.text.toString()
+            val pw = password.text.toString()
+            val ageOrAddress = age.text.toString()
+            val phoneNumber = phoneNum.text.toString()
+            if(checkInput(id = id, name = name,
+                    pw = pw, ageOrAddress = ageOrAddress,
+                    phoneNumber = phoneNumber)) {
+                signUpRequest(
+                    id = id, name = name,
+                    pw = pw, ageOrAddress = ageOrAddress,
+                    phoneNumber = phoneNumber,
+                    isManager = managerMode
+                )
+            }
+            else{
+                Log.d("err","input not found")
+            }
             it.findNavController().navigate(R.id.nav_signIn)
         }
 
@@ -64,5 +86,26 @@ class SignUpFragment : Fragment() { /*회원가입*/
             age.hint = "나이"
             title_SignUP.text = "회원가입"
         }
+    }
+
+    /*회원가입요청*/
+    private fun signUpRequest(phoneNumber : String, name : String,
+                              id : String, pw : String,
+                              ageOrAddress : String, isManager : Boolean){
+        when(isManager){
+            true->{
+                Thread{callMethodCompSignUp(name, id, pw, ageOrAddress, phoneNumber)}.run()
+
+            }
+            false->{
+                Thread{callMethodCustSignUp(phoneNumber, name, id, pw, Uint32(ageOrAddress.toLong()))}.run()
+            }
+        }
+    }
+
+    private fun checkInput(phoneNumber : String = "", name : String = "",
+                           id : String = "", pw : String = "",
+                           ageOrAddress : String = ""): Boolean {
+        return (phoneNumber != "") && (name != "") && (id != "")&& (pw != "") && (ageOrAddress != "")
     }
 }
